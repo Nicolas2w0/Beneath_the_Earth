a9d589c5c51ed8196f83113bd39bdd06c7c5e648
Nicolas Aravena <nicolas.289022@alunosatc.edu.br>
2025-11-04 23:36:55 -0300

    Personagem movimento
    animacao da movimentação do personagem, juntamente com a propria mecanica de movimentacao

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rigd;
+    public float speed; //colocar velocidade no boneco
+
+    public float jumpForce = 7f;
+    public bool isground; //verificar se ta no ch�o
+
+    public Vector2 PosicaoInicial;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigd = GetComponent<Rigidbody2D>();
+        PosicaoInicial = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isground = true;
+        }
+    }
+    public void RestartPosition()
+    {
+        transform.position = PosicaoInicial;
+    }
+    void Move()
+    {
+        float teclas = Input.GetAxis("Horizontal");
+        rigd.linearVelocity = new Vector2(teclas * speed, rigd.linearVelocity.y);
+
+        if (teclas > 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (teclas < 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        if (teclas != 0)
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isground == true)
+        {
+            rigd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isground = false;
+            anim.SetBool("IsJumping", true);
+        }
+        else
+        {
+            anim.SetBool("IsJumping", false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Diamante"))
+        {
+
+        }
+    }
+}
